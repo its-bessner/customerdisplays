3bffa04f1bb43ccb17aa722ca2a8869e0afd2157
Harrie <hbessner@googlemail.com>
2024-01-26 22:00:26 +0100

    puppet 001

---
+++ puppet/config.pp
@@ -0,0 +1,33 @@
+## Boot to Console
+file { '/etc/systemd/system/default.target':
+  ensure => 'link',
+  mode   => '0777',
+  owner  => 'root',
+  target => '/lib/systemd/system/multi-user.target',
+}
+
+## No Autologin on Console
+file { '/etc/systemd/system/getty@tty1.service.d/autologin.conf':
+  ensure => "absent",
+}
+
+# Autologin on Desktop
+file { '/etc/lightdm/lightdm.conf':
+  ensure  => 'file',
+  content => template('/home/baydev/puppet/lightdm.conf')
+}
+
+# Ensure git is present
+package { 'git':
+  ensure => 'present',
+}
+
+# Boot Settings (This file is linked to /boot/config.txt)
+file { '/boot/firmware/config.txt':
+  ensure  => 'file',
+  owner   => 'root',
+  mode    => '0755',
+  content => template('/home/baydev/puppet/config.txt')
+}
+
+
