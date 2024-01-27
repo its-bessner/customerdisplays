a16d6ba45c61f08cd7b0a05b297fb04e30950760
Harrie <hbessner@googlemail.com>
2024-01-27 09:29:36 +0100

    Refactor Puppet config.pp file into multiple classes
    The changes include packaging related code inside respective classes. The 'systemd', 'lightdm', 'packages', 'boot_settings', 'puppet_reboot_module', and 'reboot_system' functionalities have been transformed into individual classes, improving code organization and readability.

--- puppet/config.pp
+++ puppet/config.pp
@@ -1,44 +1,56 @@
-## Boot to Console
-file { '/etc/systemd/system/default.target':
-  ensure => 'link',
-  mode   => '0777',
-  owner  => 'root',
-  target => '/lib/systemd/system/multi-user.target',
+class mymodule::systemd {
+  # Boot to Console
+  file { '/etc/systemd/system/default.target':
+    ensure => 'link',
+    mode   => '0777',
+    owner  => 'root',
+    target => '/lib/systemd/system/multi-user.target',
+  }
+
+  # No Autologin on Console
+  file { '/etc/systemd/system/getty@tty1.service.d/autologin.conf':
+    ensure => "absent",
+  }
 }
 
-## No Autologin on Console
-file { '/etc/systemd/system/getty@tty1.service.d/autologin.conf':
-  ensure => "absent",
+class mymodule::lightdm {
+  # Autologin on Desktop
+  file { '/etc/lightdm/lightdm.conf':
+    ensure  => 'file',
+    content => template('mymodule/lightdm.conf.erb'),
+  }
 }
 
-# Autologin on Desktop
-file { '/etc/lightdm/lightdm.conf':
-  ensure  => 'file',
-  content => template('/home/baydev/puppet/lightdm.conf')
+class mymodule::packages {
+  # Ensure git is present
+  package { 'git': ensure => 'present' }
+
+  # mutt
+  package { 'mutt': ensure => 'installed' }
 }
 
-# Ensure git is present
-package { 'git':
-  ensure => 'present',
+class mymodule::boot_settings {
+  # Boot Settings (This file is linked to /boot/config.txt)
+  file { '/boot/firmware/config.txt':
+    ensure  => 'file',
+    owner   => 'root',
+    mode    => '0755',
+    content => template('mymodule/config.txt.erb'),
+  }
 }
 
-# Boot Settings (This file is linked to /boot/config.txt)
-file { '/boot/firmware/config.txt':
-  ensure  => 'file',
-  owner   => 'root',
-  mode    => '0755',
-  content => template('/home/baydev/puppet/config.txt')
+class mymodule::puppet_reboot_module {
+  # puppet module 'reboot'
+  exec { 'install-reboot-module':
+    command => '/opt/puppetlabs/bin/puppet module install puppetlabs/reboot',
+    unless  => '/opt/puppetlabs/bin/puppet module list | grep reboot',
+    path    => ['/usr/bin', '/usr/sbin', '/usr/bin'],
+  }
 }
 
-# mutt
-package { 'mutt': ensure => 'installed' }
-
-
-# puppet module 'reboot'
-exec { 'install-reboot-module':
-  command => '/usr/bin/puppet module install puppetlabs/reboot',
-  unless  => '/usr/bin/puppet module list | grep reboot',
-  path    => ['/bin', '/usr/sbin', '/usr/bin'],
+class mymodule::reboot_system {
+  reboot { 'reboot_after_run':
+    message => 'The system is going down for reboot now!',
+    when    => 'finished',
+  }
 }
-
-
