f899a0d1d0d965ffea7b97ffd3608878ab235ee0
Harrie <hbessner@googlemail.com>
2024-01-27 10:02:44 +0100

    Correct conditional reboot in checkGit.sh
    The checkGit.sh script has been updated to correctly execute a conditional reboot command only when arguments are provided. The echo statement has been removed to clean up the script's output during running.

--- checkGit.sh
+++ checkGit.sh
@@ -4,7 +4,6 @@ git fetch origin master
 if [ -n "$(git diff origin/master)" ]; then
   git pull origin master
   sudo puppet apply puppet/config.pp
+  if [ -n "$1" ]; then sudo reboot; fi
 #  sudo reboot
 fi
-
-echo S1: "$1"
