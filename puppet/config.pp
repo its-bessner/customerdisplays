9b228a2bed56870ec70b948ea609139ca2ee2a45
Harrie <hbessner@googlemail.com>
2024-01-27 20:50:35 +0100

    Refactored puppet configuration file structure
    Removed separate class definitions in the puppet configuration. The classes were used to delineate different configuration aspects but did not contribute any functional benefit to the configuration process. The simplified structure has reduced clutter and made the configuration easier to read and modify.

--- puppet/config.pp
+++ puppet/config.pp
@@ -1,4 +1,3 @@
-class mymodule::systemd {
   # Boot to Console
   file { '/etc/systemd/system/default.target':
     ensure => 'link',
@@ -11,25 +10,19 @@ class mymodule::systemd {
   file { '/etc/systemd/system/getty@tty1.service.d/autologin.conf':
     ensure => "absent",
   }
-}
 
-class mymodule::lightdm {
   # Autologin on Desktop
   file { '/etc/lightdm/lightdm.conf':
     ensure  => 'file',
     content => template('mymodule/lightdm.conf.erb'),
   }
-}
 
-class mymodule::packages {
   # Ensure git is present
   package { 'git': ensure => 'present' }
 
   # mutt
   package { 'mutt': ensure => 'installed' }
-}
 
-class mymodule::boot_settings {
   # Boot Settings (This file is linked to /boot/config.txt)
   file { '/boot/firmware/config.txt':
     ensure  => 'file',
@@ -37,32 +30,25 @@ class mymodule::boot_settings {
     mode    => '0755',
     content => template('config.txt'),
   }
-}
 
-class mymodule::puppet_reboot_module {
   # puppet module 'reboot'
   exec { 'install-reboot-module':
     command => '/opt/puppetlabs/bin/puppet module install puppetlabs/reboot',
     unless  => '/opt/puppetlabs/bin/puppet module list | grep reboot',
     path    => ['/usr/bin', '/usr/sbin', '/usr/bin'],
   }
-}
 
 
-class install_cron_module {
   exec { 'install-cron-module':
     path    => '/opt/puppetlabs/bin:/usr/bin:/usr/sbin',
     command => 'puppet module install puppetlabs-cron_core',
-    unless  => 'puppet module list | grep cron_core',
+    unless  => '/puppet module list | grep cron_core',
   }
-}
 
 
 
-class mymodule::cron{
   cron {'checkGit':
     special =>  'reboot',
     user => 'baydev',
     command => '/home/baydev/checkGit.sh reload'
   }
-}
