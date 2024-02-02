0fdecce904cad159b9dec1c72de42f1b173aa585
Harrie <hbessner@googlemail.com>
2024-02-02 17:56:30 +0100

    Update checkGit.sh script to v118
    This commit updates the script 'checkGit.sh' to version 118 and adds better usage instructions for the "reboot" and "batch" parameters. Furthermore, it optimizes the check for new commits, by using 'git remote show' instead of the more inefficient fetch and diff strategy.

--- checkGit.sh
+++ checkGit.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
-# v117
+# v118
+
+# usage: checkGit.sh [reboot|batch]
+# reboot as first param: restart after apply puppet manifests (this is the default value for cron@reboot)
+# batch as first param: apply puppet manifests even if there are no changes from local to remote branch
 
 if [ -n "$(git remote show origin 2>/dev/null | grep 'master' | grep 'out of date')" ] || [ "$1" = 'batch' ]; then
   git pull origin master
