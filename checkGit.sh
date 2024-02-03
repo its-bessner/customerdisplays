09136bf4787f109aba93c8d55bdf87cd8e07cf23
Harrie <hbessner@googlemail.com>
2024-02-03 08:12:45 +0100

    Apply puppet manifests at every reboot - even if there a no changes

--- checkGit.sh
+++ checkGit.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
-# v118
+# v119
 
 # usage: checkGit.sh [reboot|batch]
 # reboot as first param: restart after apply puppet manifests (this is the default value for cron@reboot)
 # batch as first param: apply puppet manifests even if there are no changes from local to remote branch
+# No Param at all: just apply the manifest
 
 if [ -n "$(git remote show origin 2>/dev/null | grep 'master' | grep 'out of date')" ] || [ "$1" = 'batch' ]; then
   git pull origin master
   sudo puppet apply puppet/base.pp
   sudo puppet apply puppet/config.pp
   if [ "$1" = "reboot" ]; then sudo reboot; fi
+else
+  sudo puppet apply puppet/base.pp
+  sudo puppet apply puppet/config.pp
 fi
